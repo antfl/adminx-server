0120b625e91e97c48f5a0bd7ab7efbe89fa98b3c
GavinHaydy <13558622779@163.com>
2025-06-28 09:44:09 +0800

    feat：email code validate

--- src/main/java/com/bytescheduler/adminx/modules/system/service/AuthService.java
+++ src/main/java/com/bytescheduler/adminx/modules/system/service/AuthService.java
@@ -1,9 +1,6 @@
 package com.bytescheduler.adminx.modules.system.service;
 
-import com.bytescheduler.adminx.modules.system.dto.CaptchaResponse;
-import com.bytescheduler.adminx.modules.system.dto.LoginRequest;
-import com.bytescheduler.adminx.modules.system.dto.RegisterRequest;
-import com.bytescheduler.adminx.modules.system.dto.TokenResponse;
+import com.bytescheduler.adminx.modules.system.dto.*;
 
 /**
  * @author byte-scheduler
@@ -16,4 +13,6 @@ public interface AuthService {
     TokenResponse login(LoginRequest request);
 
     CaptchaResponse generateCaptcha();
+
+    MailCodeResponse generateMailCode(String email);
 }
--- src/main/java/com/bytescheduler/adminx/modules/system/service/impl/AuthServiceImpl.java
+++ src/main/java/com/bytescheduler/adminx/modules/system/service/impl/AuthServiceImpl.java
@@ -5,13 +5,12 @@ import cn.hutool.captcha.LineCaptcha;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.bytescheduler.adminx.common.exception.BusinessException;
 import com.bytescheduler.adminx.common.utils.JwtTokenUtil;
-import com.bytescheduler.adminx.modules.system.dto.CaptchaResponse;
-import com.bytescheduler.adminx.modules.system.dto.LoginRequest;
-import com.bytescheduler.adminx.modules.system.dto.RegisterRequest;
-import com.bytescheduler.adminx.modules.system.dto.TokenResponse;
+import com.bytescheduler.adminx.common.utils.MailUtil;
+import com.bytescheduler.adminx.modules.system.dto.*;
 import com.bytescheduler.adminx.modules.system.entity.User;
 import com.bytescheduler.adminx.modules.system.mapper.SysUserMapper;
 import com.bytescheduler.adminx.modules.system.service.AuthService;
+import com.bytescheduler.adminx.security.config.EmailConfig;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.BeanUtils;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -38,10 +37,35 @@ public class AuthServiceImpl implements AuthService {
     private final JwtTokenUtil jwtTokenUtil;
     private final RedisTemplate<String, String> redisTemplate;
     private final CaptchaConfig captchaConfig;
+    private final EmailConfig emailConfig;
+    private final MailUtil mailUtil;
 
     @Override
     @Transactional
     public void register(RegisterRequest request) {
+        //  判断是否走邮箱验证
+        if (emailConfig.isVerificationEnabled()){
+            String email = request.getEmail();
+            String captchaId = email +  emailConfig.getKeyHead() + request.getCaptchaId();
+            String rds_code = redisTemplate.opsForValue().get(captchaId);
+            String code = request.getCode();
+
+            if (emailConfig.isIgnoreCase()) {
+                // 忽略大小写比较
+                if (rds_code == null || !rds_code.equalsIgnoreCase(code)) {
+                    redisTemplate.delete(captchaId);
+                    throw new BusinessException("验证码错误");
+                }
+            } else {
+                // 严格区分大小写
+                if (rds_code == null || !rds_code.equals(code)) {
+                    redisTemplate.delete(captchaId);
+                    throw new BusinessException("验证码错误");
+                }
+            }
+
+        }
+
         QueryWrapper<User> queryWrapper = new QueryWrapper<>();
         queryWrapper.eq("email", request.getEmail());
         if (userMapper.selectCount(queryWrapper) > 0) {
@@ -62,20 +86,24 @@ public class AuthServiceImpl implements AuthService {
     public TokenResponse login(LoginRequest request) {
         String identifier = request.getUsername().trim();
         String captchaId = captchaConfig.getKeyHead() + request.getCaptchaId();
-        String code;
-        String rds_code;
 
-        //  获取验证码
-        if (captchaConfig.isIgnoreCase()){
-            code = request.getCode().toLowerCase();
-            rds_code = Objects.requireNonNull(redisTemplate.opsForValue().get(captchaId)).toLowerCase();
-        }else {
-            code = request.getCode();
-            rds_code = redisTemplate.opsForValue().get(captchaId);
+        // 获取 Redis 中验证码
+        String rds_code = redisTemplate.opsForValue().get(captchaId);
+        if (rds_code == null) {
+            throw new BusinessException("验证码不存在或已过期");
+        }
+
+        // 处理忽略大小写
+        String code = request.getCode();
+        if (captchaConfig.isIgnoreCase()) {
+            if (code != null) {
+                code = code.toLowerCase();
+            }
+            rds_code = rds_code.toLowerCase();
         }
 
         redisTemplate.delete(captchaId);
-        if (!Objects.equals(rds_code, code) || Objects.isNull(rds_code)) {
+        if (!Objects.equals(rds_code, code)) {
             throw new BusinessException("验证码错误");
         }
 
@@ -135,5 +163,25 @@ public class AuthServiceImpl implements AuthService {
         // 返回 Base64 图片 + ID
         String base64 = captcha.getImageBase64Data();
         return new CaptchaResponse(captchaId, base64);
+    }
+
+    @Override
+    public MailCodeResponse generateMailCode(String email) {
+        // 生成验证码
+        String code = mailUtil.generateComplexCode(emailConfig.getCodeCount());
+
+        // 验证码唯一 ID
+        String captchaId = UUID.randomUUID().toString();
+
+        // 存入 Redis（300 秒过期）
+        redisTemplate.opsForValue().set(
+                email + emailConfig.getKeyHead() + captchaId, // 拼接email保证接收地址和使用地址相同
+                code,
+                Duration.ofSeconds(emailConfig.getExpireSeconds())
+        );
+        mailUtil.sendVerifyCode(email,code);
+
+        // 返回 Code ID
+        return new MailCodeResponse(captchaId);
     }
 }
