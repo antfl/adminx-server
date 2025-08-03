e97868be670dde06baf0a270ccc047b8a17fe47a
格子 <fl9420@qq.com>
2025-08-03 22:59:44 +0800

    chore: 优化发送邮箱规则

--- adminx-core/src/main/java/com/bytescheduler/adminx/common/utils/MailUtil.java
+++ adminx-core/src/main/java/com/bytescheduler/adminx/common/utils/MailUtil.java
@@ -1,5 +1,6 @@
 package com.bytescheduler.adminx.common.utils;
 
+import com.bytescheduler.adminx.common.exception.BusinessException;
 import com.bytescheduler.adminx.repository.config.EmailConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
@@ -29,13 +30,16 @@ public class MailUtil {
     /**
      * 发送邮箱验证码
      *
-     * @param to   收件人邮箱
-     * @param code 验证码
+     * @param email 收件人邮箱
+     * @param code  验证码
      */
-    public void sendVerifyCode(String to, String code) {
+    public void sendVerifyCode(String email, String code) {
+        if (!EmailValidator.isTrustedEmail(email)) {
+            throw new BusinessException("请使用常规邮箱");
+        }
         SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom(String.format(emailConfig.getTeamName(), from));
-        message.setTo(to);
+        message.setTo(email);
         message.setSubject(emailConfig.getSubject());
         message.setText(String.format(emailConfig.getContentTemplate(), code));
         mailSender.send(message);
